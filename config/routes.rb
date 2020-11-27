48e24382597f00252e48cdf1945920c751232efd
Abasov <C:\Users\Abasov\AppData\Roaming\The Bat!>
2020-11-27 13:16:20 +1000

    Add one-to-many article-comment

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
 
   resources :articles
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
