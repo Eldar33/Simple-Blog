70c6c04d8c480224610532089b9d82f5984c4b73
Abasov <C:\Users\Abasov\AppData\Roaming\The Bat!>
2020-12-04 13:22:26 +1000

    add test for last_comment

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,16 @@ FactoryBot.define do
   factory :article do
     title { "Article title" }
     text { "Article text" }
+
+    #создаем фабрику "article_with_comments"
+    #для создания статьи с несколькими комметариями
+    factory :article_with_comments do
+      # После создания artcle
+      after(:create) do |article, evaluator|
+        # создаем список из 3-х комментариев
+        create_list :comment, 3, article: article
+      end
+    end
+
   end
 end
