51b71d3550a1f5f1a07fd19986be190a45350eea
ELdar33 <eldar_elf_33@mail.ru>
2020-12-07 20:58:19 +1000

    rename first scenario & add second scenario for save create article

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -3,10 +3,23 @@ require 'support/session_helper'
 
 feature 'Article creation' do
 
-  scenario 'allows user to create article' do    
+  before(:each) do
     sign_up
+  end
+
+  scenario 'allows user to open new article' do
     visit new_article_path
     expect(page).to have_content 'New article'
+  end
+
+  scenario 'allows user to create article' do
+
+    visit new_article_path
+    fill_in :article_title, with: "Test article title"
+    fill_in :article_text, with: "Test article text"
+    click_button 'Save Article'
+    expect(page).to have_content 'Test article title'
+
   end
 
 end
