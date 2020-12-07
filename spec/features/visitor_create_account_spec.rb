ca1705c670de3c9f132a5a29eea452460f382685
Abasov <C:\Users\Abasov\AppData\Roaming\The Bat!>
2020-12-07 13:27:12 +1000

    refactor sign up test

--- spec/features/visitor_create_account_spec.rb
+++ spec/features/visitor_create_account_spec.rb
@@ -1,16 +1,12 @@
 require 'rails_helper'
+require 'support/session_helper'
 
 feature 'Account creation' do
 
-  scenario 'allows guest to create account' do
-    visit new_user_registration_path
-    fill_in :user_email, with: 'user@example.com'
-    fill_in :user_username, with: 'test_user'
-    fill_in :user_password, with: '123456'
-    fill_in :user_password_confirmation, with: '123456'
-    click_button 'Sign up'
-
+  scenario 'allows guest to create account' do    
+    sign_up
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
 
 end
+
