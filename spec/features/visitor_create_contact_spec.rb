5fab035c8017c0a65854da47bad100b85861e3b8
Abasov <C:\Users\Abasov\AppData\Roaming\The Bat!>
2020-12-07 12:25:01 +1000

    update create contact test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 feature 'Contact Cretion' do
   scenario 'allows acees to contacts page' do
-    visit '/contacts'
-    expect(page).to have_content 'Contact us'
+    # visit '/contacts'
+    visit contacts_path
+    expect(page).to have_content I18n.t('contacts.contact_us')
   end
 end
