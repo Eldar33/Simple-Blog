acab75194227112b162e88f70cfe4baabeb10c2b
Abasov <C:\Users\Abasov\AppData\Roaming\The Bat!>
2020-12-07 12:44:07 +1000

    update tests (allow a guest to create contact)

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 feature 'Contact Cretion' do
-  scenario 'allows acees to contacts page' do
+  scenario 'allows accees to contacts page' do
     # visit '/contacts'
     visit contacts_path
     expect(page).to have_content I18n.t('contacts.contact_us')
+  end
+
+  scenario 'allows a guest to create contacts' do
+    visit contacts_path
+    fill_in :contact_email, with: 'user@example.com'
+    fill_in :contact_message, with: 'Blabla'
+    click_button 'Send message'
+    expect(page).to have_content 'Thanks!'
   end
 end
